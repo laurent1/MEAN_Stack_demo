16efb6745c71faf1f6cffb1f12c9847d789f442a
Laurent Jacquot <laurentjacquot1@yahoo.com>
2015-01-28 16:12:37 -0800

    Moves authenticate logic from the routes to its own file

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,7 +1,7 @@
 /**
  * Created by laurentMac on 1/27/15.
  */
-var passport = require('passport');
+var auth = require('./auth');
 
 module.exports = function (app) {
   // This would. And the files were moved to /public/app/ as well
@@ -9,18 +9,7 @@ module.exports = function (app) {
     res.render('../../public/app/' + req.params[0]);
   });
 
-  app.post('/login', function (req, res, next) {
-    var auth = passport.authenticate('local', function (err, user) {
-      if(err) {return next(err);}
-      if(!user) { res.send({success:false})}
-      // because we are using xhr (and not a server side route, which is a submitted form)
-      req.logIn(user, function(err) {
-        if(err) {return next(err);}
-        res.send({success:true, user: user});
-      })
-    })
-    auth(req, res, next);
-  });
+  app.post('/login', auth.authenticate);
 
   // * matches all routes
   app.get('*', function(req, res) {
