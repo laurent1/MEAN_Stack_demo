66159ae47a3b6d347ab1368227142ad29c5d27e5
Laurent Jacquot <laurentjacquot1@yahoo.com>
2015-01-29 14:56:29 -0800

    Implements server-side authorization

--- server/config/routes.js
+++ server/config/routes.js
@@ -1,9 +1,19 @@
 /**
  * Created by laurentMac on 1/27/15.
  */
-var auth = require('./auth');
+var auth = require('./auth'),
+  mongoose = require('mongoose'),
+  User = mongoose.model('User');
 
 module.exports = function (app) {
+
+  // if requiresRole call next(), the second (next) function is called
+  app.get('/api/users', auth.requiresRole('admin'), function(req, res) {
+    User.find({}).exec(function(err, collection) {
+      res.send(collection);
+    })
+  });
+
   // This would. And the files were moved to /public/app/ as well
   app.get('/partials/*', function(req, res) {
     res.render('../../public/app/' + req.params[0]);
