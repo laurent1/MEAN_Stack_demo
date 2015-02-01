aa05759bc89ae6c994277aa230e972140850c553
Laurent Jacquot <laurentjacquot1@yahoo.com>
2015-01-31 18:35:12 -0800

    Getting cached data for course details pages

--- public/app/courses/mvCourseDetailCtrl.js
+++ public/app/courses/mvCourseDetailCtrl.js
@@ -1,7 +1,13 @@
 /**
  * Created by laurentMac on 1/31/15.
  */
-angular.module('app').controller('mvCourseDetailCtrl', function($scope, mvCourse, $routeParams) {
-  $scope.course = mvCourse.get({_id:$routeParams.id})
-
+angular.module('app').controller('mvCourseDetailCtrl', function($scope, mvCachedCourses, $routeParams) {
+  // Will get the course only from the course list.
+  mvCachedCourses.query().$promise.then(function(collection) {
+    collection.forEach(function(course) {
+      if(course._id === $routeParams.id) {
+        $scope.course = course;
+      }
+    })
+  })
 });
